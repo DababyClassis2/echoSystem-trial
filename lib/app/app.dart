a99c68586077a3d8e72778a7f2351944393c8913
DababyClassis2 <dababyclassis2@gmail.com>
2026-05-17 15:21:57 +0400

    feat(android): add complete manifest with permissions and FileProvider

---
+++ lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'router.dart';
+import 'theme.dart';
+
+class EchoSystemApp extends StatelessWidget {
+  const EchoSystemApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      title: 'echoSystem',
+      theme: appTheme,
+      routerConfig: appRouter,
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
