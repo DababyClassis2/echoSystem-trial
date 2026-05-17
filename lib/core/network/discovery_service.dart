ec6831af4207af28b28894d0815da3f29d9dadab
DababyClassis2 <dababyclassis2@gmail.com>
2026-05-17 18:50:25 +0400

    feat(network): stub DiscoveryService to unblock build

--- lib/core/network/discovery_service.dart
+++ lib/core/network/discovery_service.dart
@@ -3,15 +3,9 @@ import 'package:nsd/nsd.dart';
 import '../models/device_model.dart';
 
 class DiscoveryService {
-  static const String SERVICE_TYPE = '_echosystem._tcp.';
   final String deviceId;
   final String deviceName;
   final int port;
-
-  NsdService? _service;
-  NsdDiscovery? _discovery;
-  final StreamController<List<DeviceModel>> _devicesController = StreamController<List<DeviceModel>>.broadcast();
-  final Map<String, DeviceModel> _devices = {};
 
   DiscoveryService({
     required this.deviceId,
@@ -19,54 +13,14 @@ class DiscoveryService {
     required this.port,
   });
 
-  Stream<List<DeviceModel>> get devices => _devicesController.stream;
+  Stream<List<DeviceModel>> get devices => const Stream.empty();
 
   Future<void> start() async {
-    await _advertise();
-    await _discover();
-  }
-
-  Future<void> _advertise() async {
-    final txt = {
-      'deviceId': deviceId,
-      'deviceName': deviceName,
-      'port': port.toString(),
-    };
-    _service = NsdService(
-      serviceName: deviceName,
-      serviceType: SERVICE_TYPE,
-      port: port,
-      attributes: txt,
-    );
-    await _service!.register();
-  }
-
-  Future<void> _discover() async {
-    _discovery = NsdDiscovery(serviceType: SERVICE_TYPE);
-    _discovery!.stream.listen((event) {
-      final info = event.serviceInfo;
-      final txt = info.attributes;
-      if (info.host == null) return;
-      final device = DeviceModel(
-        id: txt['deviceId'] ?? info.name,
-        name: txt['deviceName'] ?? info.name,
-        ipAddress: info.host!,
-        port: int.tryParse(txt['port'] ?? '0') ?? 0,
-        isOnline: true,
-        lastSeen: DateTime.now(),
-      );
-      _devices[info.name] = device;
-      _devicesController.add(_devices.values.toList());
-    }, onError: (e) {
-      // log error
-    });
-    await _discovery!.start();
+    // Stub
   }
 
   Future<void> stop() async {
-    await _service?.unregister();
-    await _discovery?.stop();
-    await _devicesController.close();
+    // Stub
   }
 
   void dispose() {
